fa91b885495e2639fc6213cd1e7fcbb428a2e6a6
MustLuikH7 <mokamartin3@gmail.com>
2026-09-16 15:25:37 +0300

    Add haeder

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <div className="header">
+            <div className="header-wrapper">
+                <h2>Anna meile tagasiside</h2>
+            </div>
+        </div>
+    )
+}
+export default Header
