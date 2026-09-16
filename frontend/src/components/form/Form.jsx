776c6b81bb5e304bf7fafa84ef905b9076cd5ebb
MustLuikH7 <mokamartin3@gmail.com>
2026-09-16 16:00:03 +0300

    add teachers to form

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -1,9 +1,15 @@
+import teachers from "../../../../teachers.json" with { type: 'json' };
 function Form() {
     return (
         <form action="submit">
             <select name="selectSubject">
                 <option value="Matemaatika">Matemaatika</option>
                 {/* lisa koik ylejaanud */}
+            </select>
+            <select name="selectTeacher">
+                {teachers.map((teacher) =>
+                    <option value={teacher.fullName}>{teacher.fullName}</option>
+                )}
             </select>
             <select name="grade">
                 <option value="1">1</option>
